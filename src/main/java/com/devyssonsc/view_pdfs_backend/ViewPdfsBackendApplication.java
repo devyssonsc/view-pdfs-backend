06c0e433bd42fe5b8f11cc38bceaf68d281ff182
Devysson Cardoso <devyssonsc@gmail.com>
2024-08-23 17:47:13 +0100

    create project

---
+++ src/main/java/com/devyssonsc/view_pdfs_backend/ViewPdfsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devyssonsc.view_pdfs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewPdfsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewPdfsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devyssonsc/view_pdfs_backend/ViewPdfsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyssonsc.view_pdfs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewPdfsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
